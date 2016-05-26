f69209a3422199cc7b62f6fd5b1fb82c7bb765bc
Chris Myers <chris.myers@rea-group.com>
2016-05-27 09:30:59 +1000

    Adds Option Syntax for getOrEmpty.
    Adds scala compiler options to remove warnings and keep things a little tighter.

--- shared/src/main/scala/com/github/benhutchison/mouse/option.scala
+++ shared/src/main/scala/com/github/benhutchison/mouse/option.scala
@@ -1,5 +1,7 @@
 package com.github.benhutchison.mouse
 
+import cats.Monoid
+
 trait OptionSyntax {
   implicit final def optionSyntax[A](oa: Option[A]): OptionOps[A] = new OptionOps(oa)
 }
@@ -7,5 +9,11 @@ trait OptionSyntax {
 final class OptionOps[A](val oa: Option[A]) extends AnyVal {
 
   def cata[B](some: A => B, none: => B): B = oa.fold[B](none)(some)
+
+  /** Returns the containing A or the empty value for that A if A is a Monoid.
+    * For instance if A is of type String it will return the empty String if the
+    * Option is a None.
+    */
+  def getOrEmpty(implicit m: Monoid[A]): A = oa.getOrElse(m.empty)
 
 }
