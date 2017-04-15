e1aa3e26b1f7b67161d50a59426def0d6462b76d
Kailuo Wang <kailuo.wang@gmail.com>
2017-04-14 21:07:48 -0400

    added right and left to Option Ops

--- shared/src/test/scala/mouse/OptionSyntaxTest.scala
+++ shared/src/test/scala/mouse/OptionSyntaxTest.scala
@@ -1,6 +1,7 @@
 package mouse
 
 import scala.util.{Failure, Success}
+import cats.implicits._
 
 class OptionSyntaxTest extends MouseSuite {
 
@@ -14,5 +15,20 @@ class OptionSyntaxTest extends MouseSuite {
   val ex = new RuntimeException("Err")
   Option.empty[Int].toTry(ex) shouldEqual Failure(ex)
 
+  Option(3).right("S") shouldEqual Option(3).toRight("S")
+
+  Option(3).left("S") shouldEqual Option(3).toLeft("S")
+
+  None.right("S") shouldEqual None.toRight("S")
+
+  None.left("S") shouldEqual None.toLeft("S")
+
+  implicit class ExtraTest[A](a: A) {
+    def shouldBeA[T](implicit ev: T =:= A) = succeed
+  }
+
+  Option(3).right("S").shouldBeA[Either[String, Int]]
+
+  Option(3).left("S").shouldBeA[Either[Int, String]]
 
 }
