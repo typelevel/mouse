5c3ceb18765386df7c5555c16ace83d38d36eeff
Stephen Lazaro <stephen@earnest.com>
2017-05-01 18:36:05 -0700

    Thrush and ignore syntax
    Adds AnySyntax with |> and ignore
    Adds really simple tests for |> and ignore

---
+++ shared/src/main/scala/mouse/any.scala
@@ -0,0 +1,13 @@
+package mouse
+
+
+trait AnySyntax {
+  implicit final def optionSyntaxMouse[A](oa: A): AnyOps[A] = new AnyOps(oa)
+}
+
+final class AnyOps[A](val oa: A) extends AnyVal {
+  def |>[B] (f: A => B) = f(oa)
+  def thrush[B] (f: A => B) = f(oa)
+
+  def ignore = ()
+}
---
+++ shared/src/test/scala/mouse/AnySyntaxTest.scala
@@ -0,0 +1,22 @@
+package mouse
+
+
+class AnySyntaxTest extends MouseSuite {
+
+  true |> (!_) shouldEqual false
+
+  5 |> (_ + 44) shouldEqual 49
+
+  Some("thing") |> (_ getOrElse "that") shouldEqual "thing"
+
+  ("This" |> Function.const("that")
+          |> (_.capitalize)
+          |> Function.const("at bat")) shouldEqual "at bat"
+
+  true.ignore shouldBe ()
+
+  "anythingAtAll" |> (_.ignore) shouldBe ()
+
+  1200 |> (_*2) |> (_-5) |> (_/3) shouldBe (((1200 * 2) - 5) / 3)
+
+}
