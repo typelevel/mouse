62b023ff288161549c7dc3bcbfd9dd1cdca04ad1
sh0hei <vividbit@gmail.com>
2017-09-08 18:46:41 +0900

    remove final in final

--- shared/src/main/scala/mouse/try.scala
+++ shared/src/main/scala/mouse/try.scala
@@ -8,11 +8,11 @@ trait TrySyntax {
 
 final class TryOps[A](val ta: Try[A]) extends AnyVal {
 
-  final def cata[B](success: A => B, failure: Throwable => B): B =
+  def cata[B](success: A => B, failure: Throwable => B): B =
     ta match {
       case Success(value) => success(value)
       case Failure(error) => failure(error)
     }
 
-  @inline final def toEither: Either[Throwable, A] = cata[Either[Throwable, A]](Right(_), Left(_))
+  @inline def toEither: Either[Throwable, A] = cata[Either[Throwable, A]](Right(_), Left(_))
 }
