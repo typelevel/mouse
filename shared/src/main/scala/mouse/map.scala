c57d431b79519a74ece86125cfcb5e7717f9164e
Yannick Heiber <yannick.heiber@carjump.de>
2018-12-24 15:02:56 +0100

    Add updateKey, updateKeyF and updateKeyCombine to MapSyntax/MapOps

--- shared/src/main/scala/mouse/map.scala
+++ shared/src/main/scala/mouse/map.scala
@@ -1,5 +1,7 @@
 package mouse
 
+import cats.{Applicative, Semigroup}
+
 trait MapSyntax {
   implicit final def mapSyntaxMouse[A, B](map: Map[A, B]): MapOps[A, B] = new MapOps(map)
 }
@@ -7,4 +9,12 @@ trait MapSyntax {
 final class MapOps[A, B](private val map: Map[A, B]) extends AnyVal {
 
   def mapKeys[C](f: A => C): Map[C, B] = map.map { case (a, b) => (f(a), b) }
+
+  def updateKey(key: A, f: B => B): Map[A, B] = map.get(key).fold(map)(value => map.updated(key, f(value)))
+
+  def updateKeyF[F[_]](key: A, f: B => F[B])(implicit F: Applicative[F]): F[Map[A, B]] =
+    map.get(key).fold(F.pure(map))(value => F.map(f(value))(result => map.updated(key, result)))
+
+  def updateKeyCombine(key: A, add: B)(implicit sg: Semigroup[B]): Map[A, B] =
+    map.get(key).fold(map)(value => map.updated(key, sg.combine(value, add)))
 }
