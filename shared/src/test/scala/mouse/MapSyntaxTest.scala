a42ac2d3144f11a4505efb5fbc566c37d26cf3c2
Yannick Heiber <yannick.heiber@carjump.de>
2018-12-25 12:30:39 +0100

    Rename updateKeyX to updateAtKeyX, fix semantics of updateAtKeyCombine

--- shared/src/test/scala/mouse/MapSyntaxTest.scala
+++ shared/src/test/scala/mouse/MapSyntaxTest.scala
@@ -9,21 +9,21 @@ class MapSyntaxTest extends MouseSuite {
   Map[Int, Int]().mapKeys(identity) shouldEqual Map[Int, Int]()
 
 
-  Map(1 -> 2, 3 -> 4).updateKey(3, _ * 2) shouldEqual Map(1 -> 2, 3 -> 8)
+  Map(1 -> 2, 3 -> 4).updateAtKey(3, _ * 2) shouldEqual Map(1 -> 2, 3 -> 8)
 
-  Map(1 -> 2, 3 -> 4).updateKey(42, _ * 2) shouldEqual Map(1 -> 2, 3 -> 4)
+  Map(1 -> 2, 3 -> 4).updateAtKey(42, _ * 2) shouldEqual Map(1 -> 2, 3 -> 4)
 
-  Map(1 -> 2, 3 -> 4).updateKey(3, identity) shouldEqual Map(1 -> 2, 3 -> 4)
-
-
-  Map(1 -> 2, 3 -> 4).updateKeyCombine(3, 5) shouldEqual Map(1 -> 2, 3 -> 9)
-
-  Map(1 -> 2, 3 -> 4).updateKeyCombine(42, 5) shouldEqual Map(1 -> 2, 3 -> 4)
+  Map(1 -> 2, 3 -> 4).updateAtKey(3, identity) shouldEqual Map(1 -> 2, 3 -> 4)
 
 
-  Map(1 -> 2, 3 -> 4).updateKeyF(3, Option(_)) shouldEqual Some(Map(1 -> 2, 3 -> 4))
+  Map(1 -> 2, 3 -> 4).updateAtKeyCombine(3, 5) shouldEqual Map(1 -> 2, 3 -> 9)
 
-  Map(1 -> 2, 3 -> 4).updateKeyF(3, i => Option(i * 2)) shouldEqual Some(Map(1 -> 2, 3 -> 8))
+  Map(1 -> 2, 3 -> 4).updateAtKeyCombine(42, 5) shouldEqual Map(1 -> 2, 3 -> 4, 42 -> 5)
 
-  Map(1 -> 2, 3 -> 4).updateKeyF(42, Option(_)) shouldEqual Some(Map(1 -> 2, 3 -> 4))
+
+  Map(1 -> 2, 3 -> 4).updateAtKeyF(3, Option(_)) shouldEqual Some(Map(1 -> 2, 3 -> 4))
+
+  Map(1 -> 2, 3 -> 4).updateAtKeyF(3, i => Option(i * 2)) shouldEqual Some(Map(1 -> 2, 3 -> 8))
+
+  Map(1 -> 2, 3 -> 4).updateAtKeyF(42, Option(_)) shouldEqual Some(Map(1 -> 2, 3 -> 4))
 }
