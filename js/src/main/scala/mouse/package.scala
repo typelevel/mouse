e106067357e52bea4651c766c3efdcb85a37144f
Timothy McCarthy <ebh042@gmail.com>
2019-08-29 22:24:33 +1000

    Implement packages separately in jvm and js

---
+++ js/src/main/scala/mouse/package.scala
@@ -0,0 +1,13 @@
+package object mouse extends MouseFunctions {
+  object all extends AllSharedSyntax with AllJsSyntax
+  object any extends AnySyntax
+  object anyf extends AnyFSyntax
+  object option extends OptionSyntax
+  object boolean extends BooleanSyntax
+  object string extends StringSyntax
+  object `try` extends TrySyntax
+  object int extends IntSyntax
+  object double extends DoubleSyntax
+  object long extends LongSyntax
+  object map extends MapSyntax
+}
