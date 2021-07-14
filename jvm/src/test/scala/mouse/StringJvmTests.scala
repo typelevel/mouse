b02c2e994b5a8e336b78b5954425c53f8165f442
danicheg <e.danicheg@yandex.ru>
2021-07-14 21:49:42 +0300

    Use MUnit in StringJvmTests

--- jvm/src/test/scala/mouse/StringJvmTests.scala
+++ jvm/src/test/scala/mouse/StringJvmTests.scala
@@ -4,15 +4,12 @@ import java.net.{MalformedURLException, URI, URISyntaxException, URL}
 import cats.Eq
 import cats.syntax.all._
 import mouse.string._
-import org.scalacheck.{Arbitrary, Gen}
 
 import java.util.UUID
-import scala.util.Try
 
 class StringJvmTests extends MouseSuite {
-
   test("parseFloat") {
-    "123.1".parseFloat should ===(123.1f.asRight[NumberFormatException])
+    assertEquals("123.1".parseFloat, 123.1f.asRight[NumberFormatException])
   }
 
   test("parseURL") {
@@ -23,9 +20,12 @@ class StringJvmTests extends MouseSuite {
           x.getMessage == y.getMessage
       }
 
-    "http://example.com".parseURL should ===(new URL("http://example.com").asRight[MalformedURLException])
+    assertEquals(
+      "http://example.com".parseURL,
+      new URL("http://example.com").asRight[MalformedURLException]
+    )
 
-    "blah".parseURL should ===(new MalformedURLException("no protocol: blah").asLeft)
+   assert("blah".parseURL === new MalformedURLException("no protocol: blah").asLeft)
   }
 
   test("parseURI") {
@@ -36,22 +36,26 @@ class StringJvmTests extends MouseSuite {
           x.getMessage == y.getMessage
       }
 
-    "http://example.com".parseURI should ===(new URI("http://example.com").asRight[URISyntaxException])
+    assertEquals("http://example.com".parseURI, new URI("http://example.com").asRight[URISyntaxException])
 
-    "invalid uri".parseURI should ===(new URISyntaxException("invalid uri", "Illegal character in path at index 7").asLeft)
+    assert(
+      "invalid uri".parseURI === new URISyntaxException("invalid uri", "Illegal character in path at index 7").asLeft
+    )
   }
 
   test("parseUUID") {
     val validUUIDStr = "00000000-0000-0000-0000-000000000000"
     val invalidUUIDStr = "invalid"
 
-    validUUIDStr.parseUUID should ===(UUID.fromString(validUUIDStr).asRight[IllegalArgumentException])
-    validUUIDStr.parseUUID.toValidated should ===(validUUIDStr.parseUUIDValidated)
-    validUUIDStr.parseUUID.toOption should ===(validUUIDStr.parseUUIDOption)
+    assertEquals(validUUIDStr.parseUUID, UUID.fromString(validUUIDStr).asRight[IllegalArgumentException])
+    assertEquals(validUUIDStr.parseUUID.toValidated, validUUIDStr.parseUUIDValidated)
+    assertEquals(validUUIDStr.parseUUID.toOption, validUUIDStr.parseUUIDOption)
 
-    invalidUUIDStr.parseUUID should ===(new IllegalArgumentException("Invalid UUID string: invalid").asLeft[UUID])
-    invalidUUIDStr.parseUUID.toValidated should ===(invalidUUIDStr.parseUUIDValidated)
-    invalidUUIDStr.parseUUID.toOption.isEmpty shouldBe true
+    assert(
+      invalidUUIDStr.parseUUID === new IllegalArgumentException("Invalid UUID string: invalid").asLeft[UUID]
+    )
+
+    assert(invalidUUIDStr.parseUUID.toValidated === invalidUUIDStr.parseUUIDValidated)
+    assert(invalidUUIDStr.parseUUID.toOption.isEmpty)
   }
-
 }
