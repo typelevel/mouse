94971393922e0715bfe7f9541615c89ff0823cbf
danicheg <e.danicheg@yandex.ru>
2021-09-26 22:44:09 +0300

    Add FOptionSyntax

---
+++ shared/src/main/scala/mouse/foption.scala
@@ -0,0 +1,67 @@
+package mouse
+
+import cats.{FlatMap, Functor, Monad}
+
+trait FOptionSyntax {
+  implicit final def FOptionSyntaxMouse[F[_], A](foa: F[Option[A]]): FOptionOps[F, A] = new FOptionOps(foa)
+}
+
+final class FOptionOps[F[_], A](private val foa: F[Option[A]]) extends AnyVal {
+
+  def cata[B](default: => B, f: A => B)(implicit F: Functor[F]): F[B] =
+    F.map(foa)(_.fold(default)(f))
+
+  def cataF[B](default: => F[B], f: A => F[B])(implicit F: FlatMap[F]): F[B] =
+    F.flatMap(foa)(_.fold(default)(f))
+
+  def exists(f: A => Boolean)(implicit F: Functor[F]): F[Boolean] =
+    F.map(foa)(_.exists(f))
+
+  def existsF(f: A => F[Boolean])(implicit F: Monad[F]): F[Boolean] =
+    F.flatMap(foa) {
+      case None => F.pure(false)
+      case Some(a) => f(a)
+    }
+
+  def flatMapIn[B](f: A => Option[B])(implicit F: Functor[F]): F[Option[B]] =
+    F.map(foa)(_.flatMap(f))
+
+  def flatMapF[B](f: A => F[Option[B]])(implicit F: Monad[F]): F[Option[B]] =
+    F.flatMap(foa)(_.fold(F.pure(Option.empty[B]))(f))
+
+  def fold[B](default: => B)(f: A => B)(implicit F: Functor[F]): F[B] =
+    cata(default, f)
+
+  def foldF[B](default: => F[B])(f: A => F[B])(implicit F: FlatMap[F]): F[B] =
+    cataF(default, f)
+
+  def forall(f: A => Boolean)(implicit F: Functor[F]): F[Boolean] =
+    F.map(foa)(_.forall(f))
+
+  def forallF(f: A => F[Boolean])(implicit F: Monad[F]): F[Boolean] =
+    F.flatMap(foa) {
+      case None => F.pure(true)
+      case Some(a) => f(a)
+    }
+
+  def getOrElse[B >: A](a: => B)(implicit F: Functor[F]): F[B] =
+    F.map(foa)(_.fold(a)(identity))
+
+  def getOrElseF[B >: A](fa: => F[B])(implicit F: Monad[F]): F[B] =
+    F.flatMap(foa)(_.fold(fa)(F.pure))
+
+  def mapIn[B](f: A => B)(implicit F: Functor[F]): F[Option[B]] =
+    F.map(foa)(_.map(f))
+
+  def orElse(default: Option[A])(implicit F: Functor[F]): F[Option[A]] =
+    F.map(foa) {
+      case None => default
+      case x => x
+    }
+
+  def orElseF(defaultF: => F[Option[A]])(implicit F: Monad[F]): F[Option[A]] =
+    F.flatMap(foa) {
+      case None => defaultF
+      case x    => F.pure(x)
+    }
+}
