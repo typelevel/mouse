87e5b9d6cb3042f499a7ff22b207469d31fdbadf
danicheg <e.danicheg@yandex.ru>
2021-10-17 13:11:17 +0300

    Add fnested to all syntax

--- shared/src/main/scala/mouse/fnested.scala
+++ shared/src/main/scala/mouse/fnested.scala
@@ -2,15 +2,15 @@ package mouse
 
 import cats.{FlatMap, Functor}
 
-trait NestedSyntax {
-  implicit final def nested2SyntaxMouse[F[_], G[_], A](fga: F[G[A]]): Nested2SyntaxOps[F, G, A] =
-    new Nested2SyntaxOps[F, G, A](fga)
+trait FNestedSyntax {
+  implicit final def fnested2SyntaxMouse[F[_], G[_], A](fga: F[G[A]]): FNested2SyntaxOps[F, G, A] =
+    new FNested2SyntaxOps[F, G, A](fga)
 
-  implicit final def nested3SyntaxMouse[F[_], G[_], H[_], A](fgha: F[G[H[A]]]): Nested3SyntaxOps[F, G, H, A] =
-    new Nested3SyntaxOps[F, G, H, A](fgha)
+  implicit final def fnested3SyntaxMouse[F[_], G[_], H[_], A](fgha: F[G[H[A]]]): FNested3SyntaxOps[F, G, H, A] =
+    new FNested3SyntaxOps[F, G, H, A](fgha)
 }
 
-final class Nested2SyntaxOps[F[_], G[_], A](private val fga: F[G[A]]) extends AnyVal {
+final class FNested2SyntaxOps[F[_], G[_], A](private val fga: F[G[A]]) extends AnyVal {
   def mapNested[B](f: A => B)(implicit F: Functor[F], G: Functor[G]): F[G[B]] =
     F.map(fga)(ga => G.map(ga)(f))
 
@@ -18,7 +18,7 @@ final class Nested2SyntaxOps[F[_], G[_], A](private val fga: F[G[A]]) extends An
     F.map(fga)(ga => G.flatMap(ga)(f))
 }
 
-final class Nested3SyntaxOps[F[_], G[_], H[_], A](private val fgha: F[G[H[A]]]) extends AnyVal {
+final class FNested3SyntaxOps[F[_], G[_], H[_], A](private val fgha: F[G[H[A]]]) extends AnyVal {
   def mapNested[B](f: A => B)(implicit F: Functor[F], G: Functor[G], H: Functor[H]): F[G[H[B]]] =
     F.map(fgha)(gha => G.map(gha)(ha => H.map(ha)(f)))
 
