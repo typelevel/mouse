59ee29d93aea5f140a23822b977e8899851f9e20
danicheg <e.danicheg@yandex.ru>
2021-10-17 14:16:48 +0300

    Rename methods in the fnested syntax

--- shared/src/main/scala/mouse/fnested.scala
+++ shared/src/main/scala/mouse/fnested.scala
@@ -11,17 +11,17 @@ trait FNestedSyntax {
 }
 
 final class FNested2SyntaxOps[F[_], G[_], A](private val fga: F[G[A]]) extends AnyVal {
-  def mapNested[B](f: A => B)(implicit F: Functor[F], G: Functor[G]): F[G[B]] =
+  def mapNested2[B](f: A => B)(implicit F: Functor[F], G: Functor[G]): F[G[B]] =
     F.map(fga)(ga => G.map(ga)(f))
 
-  def flatMapNested[B](f: A => G[B])(implicit F: Functor[F], G: FlatMap[G]): F[G[B]] =
+  def flatMapNested2[B](f: A => G[B])(implicit F: Functor[F], G: FlatMap[G]): F[G[B]] =
     F.map(fga)(ga => G.flatMap(ga)(f))
 }
 
 final class FNested3SyntaxOps[F[_], G[_], H[_], A](private val fgha: F[G[H[A]]]) extends AnyVal {
-  def mapNested[B](f: A => B)(implicit F: Functor[F], G: Functor[G], H: Functor[H]): F[G[H[B]]] =
+  def mapNested3[B](f: A => B)(implicit F: Functor[F], G: Functor[G], H: Functor[H]): F[G[H[B]]] =
     F.map(fgha)(gha => G.map(gha)(ha => H.map(ha)(f)))
 
-  def flatMapNested[B](f: A => H[B])(implicit F: Functor[F], G: Functor[G], H: FlatMap[H]): F[G[H[B]]] =
+  def flatMapNested3[B](f: A => H[B])(implicit F: Functor[F], G: Functor[G], H: FlatMap[H]): F[G[H[B]]] =
     F.map(fgha)(gha => G.map(gha)(ha => H.flatMap(ha)(f)))
 }
