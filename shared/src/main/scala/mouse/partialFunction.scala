5fb42152e05c7a234953bca80953039a78c733d2
Scala Steward <me@scala-steward.org>
2021-10-30 03:45:06 +0200

    Reformat with scalafmt 3.0.8

--- shared/src/main/scala/mouse/partialFunction.scala
+++ shared/src/main/scala/mouse/partialFunction.scala
@@ -8,7 +8,7 @@ trait PartialFunctionLift {
 }
 object PartialFunctionLift {
 
-  //https://typelevel.org/cats/guidelines.html#partially-applied-type-params
+  // https://typelevel.org/cats/guidelines.html#partially-applied-type-params
   final private[mouse] class LiftEitherPartiallyApplied[A](private val dummy: Boolean = true) extends AnyVal {
     def apply[B, C](pf: PartialFunction[A, B], orElse: A => C): A => Either[C, B] =
       (a: A) => pf.lift(a).toRight(orElse(a))
