e6687cb70c0c06397643329c33a7d235eef2f421
matwojcik <mat.wojcik@gmail.com>
2021-12-13 12:33:17 +0100

    Revert "Renamed anyf lift methods to better express the target form"
    This reverts commit 37bb873b787d215bc215548539dbbb43a1e12936.

--- shared/src/main/scala/mouse/anyf.scala
+++ shared/src/main/scala/mouse/anyf.scala
@@ -13,10 +13,10 @@ final class AnyFOps[F[_], A](private val fa: F[A]) extends AnyVal {
   @inline def ||>[G[_]](f: F ~> G): G[A] = f(fa)
   @inline def thrushK[G[_]](f: F ~> G): G[A] = f(fa)
 
-  def liftRightT[E](implicit F: Functor[F]): EitherT[F, E, A] =
+  def liftEitherT[E](implicit F: Functor[F]): EitherT[F, E, A] =
     EitherT.right[E](fa)
 
-  def liftSomeT(implicit F: Functor[F]): OptionT[F, A] =
+  def liftOptionT(implicit F: Functor[F]): OptionT[F, A] =
     OptionT.liftF(fa)
 
 }
