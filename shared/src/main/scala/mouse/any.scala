376d56fb30679c33ec2f313a49cefd81b5f2f6ce
danicheg <e.danicheg@yandex.ru>
2021-12-14 15:14:59 +0300

    Rename parameter in the AnyOps

--- shared/src/main/scala/mouse/any.scala
+++ shared/src/main/scala/mouse/any.scala
@@ -6,24 +6,24 @@ trait AnySyntax {
   implicit final def anySyntaxMouse[A](oa: A): AnyOps[A] = new AnyOps(oa)
 }
 
-final class AnyOps[A](private val oa: A) extends AnyVal {
-  @inline def |>[B](f: A => B) = f(oa)
+final class AnyOps[A](private val a: A) extends AnyVal {
+  @inline def |>[B](f: A => B) = f(a)
 
-  @inline def thrush[B](f: A => B) = f(oa)
+  @inline def thrush[B](f: A => B) = f(a)
 
   @inline def <|(f: A => Unit): A = {
-    f(oa)
-    oa
+    f(a)
+    a
   }
 
   @inline def unsafeTap(f: A => Unit): A = {
-    f(oa)
-    oa
+    f(a)
+    a
   }
 
   @inline def noneF[F[_]](implicit F: Applicative[F]): F[Option[A]] =
     FOptionSyntax.noneF[F, A]
 
   @inline def someF[F[_]](implicit F: Applicative[F]): F[Option[A]] =
-    FOptionSyntax.someF[F, A](oa)
+    FOptionSyntax.someF[F, A](a)
 }
