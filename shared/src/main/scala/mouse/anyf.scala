3093d23b275002bbddf4d91def0f01cf14c3cd90
geirolz <david.geirola@gmail.com>
2024-03-12 00:04:36 +0100

    Add recover either

--- shared/src/main/scala/mouse/anyf.scala
+++ shared/src/main/scala/mouse/anyf.scala
@@ -23,8 +23,7 @@ package mouse
 
 import cats.data.EitherT
 import cats.data.OptionT
-import cats.~>
-import cats.Functor
+import cats.{Functor, MonadError, ~>}
 
 trait AnyFSyntax {
   implicit final def anyfSyntaxMouse[F[_], A](fa: F[A]): AnyFOps[F, A] = new AnyFOps(fa)
@@ -49,4 +48,13 @@ final class AnyFOps[F[_], A](private val fa: F[A]) extends AnyVal {
   def liftOptionT(implicit F: Functor[F]): OptionT[F, A] =
     OptionT.liftF(fa)
 
+  // monadError error
+  def recoverAsLeft[E, L](pf: PartialFunction[E, L])(implicit F: MonadError[F, E]): F[Either[L, A]] =
+    recoverEither(pf.andThen(Left(_)))
+
+  def recoverAsRight[E](pf: PartialFunction[E, A])(implicit F: MonadError[F, E]): F[Either[Nothing, A]] =
+    recoverEither(pf.andThen(Right(_)))
+
+  def recoverEither[E, L](pf: PartialFunction[E, Either[L, A]])(implicit F: MonadError[F, E]): F[Either[L, A]] =
+    F.recover(mapAsRight[L])(pf)
 }
